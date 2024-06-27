f8aa8ba476a5e37b2a4542df418f4e1f37614fc7
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-27 19:01:28 +0530

    setup done

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,10 +1,7 @@
-
-function App() {
-  return (
-    <>
-        Hello
-    </>
-  )
+export default function App() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
 }
-
-export default App
