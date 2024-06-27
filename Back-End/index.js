85a16c98d6f3b52a14472b337d8bc959b20eb444
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-27 18:49:05 +0530

    updated

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,1 +1,25 @@
-console.log("hello")
+// packages
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+// Utiles
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const port = process.env.PORT || 5001;
+
+connectDB()
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
