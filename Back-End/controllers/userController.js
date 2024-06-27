4f2c7b9134240b4e6331e887aed6224f42639117
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-28 00:00:32 +0530

    login & logout done in backend

--- Back-End/controllers/userController.js
+++ Back-End/controllers/userController.js
@@ -34,4 +34,42 @@ const createUser = asyncHandler(async (req, res) => {
     }
 });
 
-export { createUser };
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    console.log(email);
+    console.log(password);
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        const isPasswordValid = await bcrypt.compare(
+            password,
+            existingUser.password
+        );
+
+        if (isPasswordValid) {
+            createToken(res, existingUser._id);
+
+            res.status(201).json({
+                _id: existingUser._id,
+                username: existingUser.username,
+                email: existingUser.email,
+                isAdmin: existingUser.isAdmin,
+            });
+            return;
+        }
+    }
+});
+
+
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+    res.cookie("jwt", "", {
+        httyOnly: true,
+        expires: new Date(0),
+    });
+
+    res.status(200).json({ message: "Logged out successfully" });
+});
+
+export { createUser, loginUser, logoutCurrentUser };
--- Back-End/routes/userRoutes.js
+++ Back-End/routes/userRoutes.js
@@ -2,8 +2,12 @@ import express from "express";
 const router = express.Router();
 import {
     createUser,
+    loginUser,
+    logoutCurrentUser
 } from "../controllers/userController.js";
 
 router.route("/").post(createUser);
+router.post("/auth", loginUser);
+router.post("/logout", logoutCurrentUser);
 
 export default router;
