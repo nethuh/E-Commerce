efa678479d1535be0ce3bd252f326103dbc7ec27
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-27 22:36:35 +0530

    userModel updated

--- Back-End/index.js
+++ Back-End/index.js
@@ -18,8 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
-});
+app.use("/api/users",userRoutes);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
