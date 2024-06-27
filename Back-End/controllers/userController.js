6f5b01a49b67b51fdea924fd9da9acee69ddcb34
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-27 23:35:51 +0530

    updated

--- Back-End/controllers/userController.js
+++ Back-End/controllers/userController.js
@@ -4,9 +4,33 @@ import asyncHandler from "../middlewares/asyncHandler.js";
 
 const createUser = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
-    console.log(username);
-    console.log(email);
-    console.log(password);
+
+    if (!username || !email || !password) {
+        throw new Error("Please fill all the inputs.");
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) res.status(400).send("User already exists");
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({username , email, password})
+
+    try {
+        await newUser.save();
+        /*createToken(res, newUser._id);*/
+
+        res.status(201).json({
+            _id: newUser._id,
+            username: newUser.username,
+            email: newUser.email,
+            isAdmin: newUser.isAdmin,
+        });
+    } catch (error) {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
 });
 
 export { createUser };
