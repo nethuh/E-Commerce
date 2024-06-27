d1331c7387f74ddfe64046630d3ab5ee92aaec2f
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-28 00:11:01 +0530

    updated

--- Back-End/controllers/userController.js
+++ Back-End/controllers/userController.js
@@ -72,4 +72,10 @@ const logoutCurrentUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Logged out successfully" });
 });
 
-export { createUser, loginUser, logoutCurrentUser };
+const getAllUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.json(users);
+});
+
+
+export { createUser, loginUser, logoutCurrentUser, getAllUsers };
--- Back-End/routes/userRoutes.js
+++ Back-End/routes/userRoutes.js
@@ -1,13 +1,22 @@
 import express from "express";
-const router = express.Router();
+
 import {
     createUser,
     loginUser,
-    logoutCurrentUser
+    logoutCurrentUser,
+    getAllUsers
 } from "../controllers/userController.js";
 
-router.route("/").post(createUser);
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router
+    .route("/")
+    .post(createUser)
+    .get(authenticate, authorizeAdmin, getAllUsers);
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
+
 
 export default router;
