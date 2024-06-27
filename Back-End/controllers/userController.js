e467aac0e2f57cf2b361f91f8ea9aff4e0b0f69e
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-27 23:50:11 +0530

    token is Created

--- Back-End/controllers/userController.js
+++ Back-End/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import asyncHandler from "../middlewares/asyncHandler.js";
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
@@ -15,11 +16,11 @@ const createUser = asyncHandler(async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const newUser = new User({username , email, password})
+    const newUser = new User({username , email, password: hashedPassword })
 
     try {
         await newUser.save();
-        /*createToken(res, newUser._id);*/
+        createToken(res, newUser._id);
 
         res.status(201).json({
             _id: newUser._id,
