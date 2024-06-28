dbb6cc6b81443a64fa35558db21144df853d3d56
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-28 14:41:11 +0530

    updated

--- Front-End/src/redux/store.js
+++ Front-End/src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 import { apiSlice } from "./api/apiSlice";
-
+import authReducer from "./features/auth/authSlice";
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
-
+        auth: authReducer,
     },
 
 
