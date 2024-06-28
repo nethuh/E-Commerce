83ff2ea5cc0ce5e7162e3dc3785b034ebabacb45
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-28 14:16:21 +0530

    api devlp

--- Front-End/src/pages/Auth/Navigation.jsx
+++ Front-End/src/pages/Auth/Navigation.jsx
@@ -27,11 +27,11 @@ const Navigation = () => {
         setShowSidebar(false);
     };
 
-    return <div style={{ zIndex: 9999 }}
-                     className={`${
-                         showSidebar ? "hidden" : "flex"
-                     } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh]  fixed `}
-                     id="navigation-container"
+    return <div style={{zIndex: 9999}}
+                className={`${
+                    showSidebar ? "hidden" : "flex"
+                } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh]  fixed `}
+                id="navigation-container"
     >
 
         <div className="flex flex-col justify-center space-y-4">
@@ -39,10 +39,56 @@ const Navigation = () => {
                 to="/"
                 className="flex items-center transition-transform transform hover:translate-x-2"
             >
-                <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
+                <AiOutlineHome className="mr-2 mt-[3rem]" size={26}/>
                 <span className="hidden nav-item-name mt-[3rem]">HOME</span>{" "}
             </Link>
+
+            <Link
+                to="/shop"
+                className="flex items-center transition-transform transform hover:translate-x-2"
+            >
+                <AiOutlineShopping className="mr-2 mt-[3rem]" size={26}/>
+                <span className="hidden nav-item-name mt-[3rem]">SHOP</span>{" "}
+            </Link>
+
+            <Link to="/cart" className="flex relative">
+                <div className="flex items-center transition-transform transform hover:translate-x-2">
+                    <AiOutlineShoppingCart className="mt-[3rem] mr-2" size={26}/>
+                    <span className="hidden nav-item-name mt-[3rem]">Cart</span>{" "}
+                </div>
+            </Link>
+
+            <Link to="/favorite" className="flex relative">
+                <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
+                    <FaHeart className="mt-[3rem] mr-2" size={20}/>
+                    <span className="hidden nav-item-name mt-[3rem]">
+              Favorites
+            </span>{" "}
+
+                </div>
+            </Link>
         </div>
+
+        <ul>
+            <li>
+                <Link
+                    to="/login"
+                    className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+                >
+                    <AiOutlineLogin className="mr-2 mt-[4px]" size={26}/>
+                    <span className="hidden nav-item-name">LOGIN</span>
+                </Link>
+            </li>
+            <li>
+                <Link
+                    to="/register"
+                    className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+                >
+                    <AiOutlineUserAdd size={26}/>
+                    <span className="hidden nav-item-name">REGISTER</span>
+                </Link>
+            </li>
+        </ul>
     </div>
 
 }
