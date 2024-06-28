2a08f9880798bd92fa1aae1dee8387b81590fa10
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-28 14:07:16 +0530

    updated

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./index.css";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}/>
+            )
+            )
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <RouterProvider router={router}/>
 )
--- Front-End/src/pages/Auth/Navigation.jsx
+++ Front-End/src/pages/Auth/Navigation.jsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import {
+    AiOutlineHome,
+    AiOutlineShopping,
+    AiOutlineLogin,
+    AiOutlineUserAdd,
+    AiOutlineShoppingCart,
+} from "react-icons/ai";
+import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./Navigation.css";
+
 const Navigation = () => {
-    return <div>Navigation</div>
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    const toggleSidebar = () => {
+        setShowSidebar(!showSidebar);
+    };
+
+    const closeSidebar = () => {
+        setShowSidebar(false);
+    };
+
+    return <div style={{ zIndex: 9999 }}
+                     className={`${
+                         showSidebar ? "hidden" : "flex"
+                     } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh]  fixed `}
+                     id="navigation-container"
+    >
+
+        <div className="flex flex-col justify-center space-y-4">
+            <Link
+                to="/"
+                className="flex items-center transition-transform transform hover:translate-x-2"
+            >
+                <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
+                <span className="hidden nav-item-name mt-[3rem]">HOME</span>{" "}
+            </Link>
+        </div>
+    </div>
 
 }
 
