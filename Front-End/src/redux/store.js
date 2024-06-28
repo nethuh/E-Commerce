f9a3760a883f76a7dae9241b9829b8d6cea59d58
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-28 14:32:08 +0530

    updated

---
+++ Front-End/src/redux/store.js
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "./api/apiSlice";
+
+
+const store = configureStore({
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+
+    },
+
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
+});
+
+setupListeners(store.dispatch);
+export default store;
