d58f7e52d43bdc177de98d081c559d01745efc01
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-29 02:35:40 +0530

    category backEnd crud done

--- Back-End/controllers/categoryController.js
+++ Back-End/controllers/categoryController.js
@@ -44,8 +44,40 @@ const updateCategory = asyncHandler(async (req, res) => {
     }
 });
 
+const removeCategory = asyncHandler(async (req, res) => {
+    try {
+        const removed = await Category.findByIdAndRemove(req.params.categoryId);
+        res.json(removed);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+const listCategory = asyncHandler(async (req, res) => {
+    try {
+        const all = await Category.find({});
+        res.json(all);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json(error.message);
+    }
+});
+
+const readCategory = asyncHandler(async (req, res) => {
+    try {
+        const category = await Category.findOne({ _id: req.params.id });
+        res.json(category);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json(error.message);
+    }
+});
 
 export {
     createCategory,
     updateCategory,
+    removeCategory,
+    listCategory,
+    readCategory,
 };
