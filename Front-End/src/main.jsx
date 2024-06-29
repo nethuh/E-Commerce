98bdd7a654cf9ae7c86b16764393f6575d94f7e5
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-29 19:31:06 +0530

    cart Added

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -26,6 +26,7 @@ import ProductUpdate from "./pages/Admin/ProductUpdate.jsx";
 import Home from "./pages/Home.jsx";
 import Favorites from "./pages/Products/Favorites.jsx";
 import ProductDetails from "./pages/Products/ProductDetails.jsx";
+import Cart from "./pages/Cart.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -48,6 +49,7 @@ const router = createBrowserRouter(
             <Route index={true} path="/" element={<Home />} />
             <Route path="/favorite" element={<Favorites />} />
             <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart />} />
         </Route>
             )
             )
