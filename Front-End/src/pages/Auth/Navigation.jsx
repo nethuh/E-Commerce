a6b2c1c63e0049c6a8a92f81bc185dd42c7df8b4
Sayani nethmi <nethmihewage2002@gmail.com>
2024-06-29 19:36:47 +0530

    update

--- Front-End/src/pages/Auth/Navigation.jsx
+++ Front-End/src/pages/Auth/Navigation.jsx
@@ -17,6 +17,7 @@ import FavoritesCount from "../Products/FavoritesCount.jsx";
 
 const Navigation = () => {
     const { userInfo } = useSelector((state) => state.auth);
+    const { cartItems } = useSelector((state) => state.cart);
 
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [showSidebar, setShowSidebar] = useState(false);
@@ -77,11 +78,22 @@ const Navigation = () => {
                     <AiOutlineShoppingCart className="mt-[3rem] mr-2" size={26}/>
                     <span className="hidden nav-item-name mt-[3rem]">Cart</span>{" "}
                 </div>
+
+                <div className="absolute top-9">
+                    {cartItems.length > 0 && (
+                        <span>
+                <span className="px-1 py-0 text-sm text-white bg-pink-500 rounded-full">
+                  {cartItems.reduce((a, c) => a + c.qty, 0)}
+                </span>
+              </span>
+                    )}
+                </div>
+
             </Link>
 
             <Link to="/favorite" className="flex relative">
                 <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
-                    <FaHeart className="mt-[3rem] mr-2" size={20} />
+                    <FaHeart className="mt-[3rem] mr-2" size={20}/>
                     <span className="hidden nav-item-name mt-[3rem]">
               Favorites
             </span>{" "}
